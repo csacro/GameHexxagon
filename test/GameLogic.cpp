064c5cfc3bcccfb7378ffd9be2eb1b2877931dc3
CarolinS <carolingit@t-online.de>
2020-01-18 16:39:57 +0100

    included googletest

---
+++ test/GameLogic.cpp
@@ -0,0 +1,26 @@
+/**
+ * @author Carolin Schindler
+ */
+
+//TODO
+/*
+ * GameViewToView.h/ViewHandler.h:  bool getMoveHelp(ModelTileEnum::TileEnum tile)
+ *                                  bool move(ModelTileEnum::TileEnum moveFrom, ModelTileEnum::TileEnum moveTo)
+ * ViewToData.h/DataHandler.h:      std::string getUserId()
+ *                                  std::string getUserName()
+ *                                  std::string getLobbyId()
+ *                                  std::string getGameId()
+ *                                  bool isConnected()
+ *                                  bool isGameStartable()
+ *                                  std::list<std::string> getJoinableLobbyNames()
+ *                                  std::string getLobbyId(std::string lobbyName)
+ *                                  std::list<std::string> getLobbyStrings()
+ *                                  std::list<std::string> getGameStrings()
+ *                                  std::list<bool> getGameBooleans()
+ *                                  std::list<ModelTileEnum::TileEnum > getLastMove()
+ *                                  ModelBoard::Board getBoard()
+ *                                  bool isMoveValid(ModelTileEnum::TileEnum moveFrom, ModelTileEnum::TileEnum moveTo)
+ *                                  bool isMoveFromPossible(ModelTileEnum::TileEnum moveFrom)
+ *                                  std::list<ModelTileEnum::TileEnum > getValidDirectNeighbours(ModelTileEnum::TileEnum kachel)
+ *                                  std::list<ModelTileEnum::TileEnum > getValidSecondaryNeighbours(ModelTileEnum::TileEnum kachel)
+ */
---
+++ test/Network.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author Carolin Schindler
+ */
+
+//TODO
+/*
+ * MessageType.h:   std::string messageTypeToString(MessageType mt)
+ *                  void to_json(nlohmann::json &j, const MessageType messageType)
+ * TileEnum.h/.cpp: std::string TileEnumToString(TileEnum tileEnum)
+ *                  TileEnum StringToTileEnum(std::string s)
+ * Lobby.h/.cpp:    void from_json(const nlohmann::json &j, struct Lobby &lobby);
+ * Game.h/.cpp:     void from_json(const nlohmann::json &j, Game &game);
+ * Board.h/.cpp:    void from_json(const nlohmann::json &j, struct Board &board);
+ */
